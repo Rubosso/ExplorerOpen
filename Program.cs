4a54a4499d809b7b17aab342f13ef176a31b3bc3
Ruben André Guardado Serrano <ruben_serrano_@hotmail.com>
2020-07-24 18:22:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+
+namespace ExplorerOpen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0){
+                string arg = HttpUtility.UrlDecode(args[0]);
+                string caminho = arg.Remove(0,arg.IndexOf(':')+1);
+                Process.Start(caminho);
+            }
+        }
+    }
+}
